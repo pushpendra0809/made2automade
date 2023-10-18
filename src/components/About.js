1d705fcefa1bc2d8eb1c391775ae15aede3dd6bf
pushpendra0809 <61344242+pushpendra0809@users.noreply.github.com>
2023-10-18 15:03:10 +0530

    All Done

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,37 @@ const About = () => {
   
   return (
     <div>
-       this is about page
+    <h2>   About iNotebook</h2>
+<p>
+App Version: [0.0.2023]
+</p>
+<p>
+Developer: [iNotebook -/  Pushpendra pal]
+</p>
+
+<h3>Description:</h3>
+<p>
+iNotebook is a user-centric website  designed to streamline data management. With an intuitive interface and robust features, it provides users with the ability to: Edit Data, Delere Data , Create Data , Highly suqurity 
+</p>
+<h3>Contact Information:</h3>
+<p>
+Email: supportinotebook@gmail.com<br />
+Website: iNotebook<br></br>
+Support: +97 4488844888 <br />
+Social Media: iNotebook123@twiter.com <br /></p>
+<h3>History:</h3>
+
+<p>
+Originally created by a designer and a developer at Pushpendr Pal, iNootbook has become one of the most popular front-end frameworks and open source projects in the world.
+
+iNootbook was created at Pushpendra Pal in mid-2010 by @mdo and @fat. Prior to being an open-sourced framework, iNootbook was known as Pushpendra Pal Blueprint. A few months into development, Pushpendra Pal held its first Hack Week and the project exploded as developers of all skill levels jumped in without any external guidance. It served as the style guide for internal tools development at the company for over a year before its public release, and continues to do so today.
+
+Originally released on Friday, August 19, 2011, we’ve since had over twenty releases, including two major rewrites with v2 and v3. With iNootbook 2, we added responsive functionality to the entire framework as an optional stylesheet. Building on that with iNootbook 3, we rewrote the library once more to make it responsive by default with a mobile first approach.
+
+With iNootbook 4, we once again rewrote the project to account for two key architectural changes: a migration to Sass and the move to CSS’s flexbox. Our intention is to help in a small way to move the web development community forward by pushing for newer CSS properties, fewer dependencies, and new technologies across more modern browsers.
+
+Our latest release, iNootbook 5, focuses on improving v4’s codebase with as few major breaking changes as possible. We improved existing features and components, removed support for older browsers, dropped jQuery for regular JavaScript, and embraced more future-friendly technologies like CSS custom properties as part of our tooling.
+</p>
     </div>
   )
 }
